1e10ec20502397c046440ee32c6063dea55a46a7
Crowdbotics <team@crowdbotics.com>
2023-05-22 20:28:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hilary-home-care-40899",
+  url: "https://hilary-home-care-40899.botics.co",
+  api: "https://hilary-home-care-40899.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
